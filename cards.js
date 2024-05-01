193c9625282887d6a2788c8525b9b9880e1e7d85
DianaHang <dianavegat99@gmail.com>
2024-04-30 19:19:46 -0600

    Actualización ejercicio DOM

--- cards.js
+++ cards.js
@@ -1,43 +1,79 @@
-//En caso que no funcione getElementBy podemos seleccionarlo con document.querySelector(.siclase) y document.querySelector(#siusoel ID). En este caso sería document.querySelector(".card-container")
-const CARDS_CONTAINER = document.getElementById("card-container");
-const USER = {
+/*
+ * [X] Crear tarjeta
+ * [X] Crear secciones p/tarjeta
+ * Img
+ * Nombre, desc, edad, lista de algo
+ * [X] Insertar en la tarjeta
+ * [x] Mostrar dentro de div.container
+ * [X] Crear funciones reutilizables
+ * [X] Funciones para crear items
+ * [X] Funciones para inyectar texto
+ * [X] Funciones para renderizar
+ * [x] Multiples usuarios
+ */
+
+const CARDS_CONTAINER = document.querySelector("#card-container");
+const USERS = [
+  {
     id: 1,
-    userName: "User Name",
-    desc: "Sobre mí",
-    age: 24,
-    //Nuevo objeto anidado y a su vez va a ser un array
-    fav_music: {
-        music: ["Rap", "R&B", "Rock", "Hip-Hop","Indie rock"],
+    username: "booklover123",
+    description: "Avid reader and coffee enthusiast.",
+    age: 29,
+    fav_books: {
+      books: ["To Kill a Mockingbird", "1984", "Pride and Prejudice"],
     },
+  },
+  {
+    id: 2,
+    username: "literature_junkie",
+    description: "Poetry lover and amateur writer.",
+    age: 35,
+    fav_books: {
+      books: ["The Great Gatsby", "Invisible Man", "Beloved"],
+    },
+  },
+];
 
-};
+// Crear secciones
+function createElements() {
+  const cardObj = {
+    name_section: document.createElement("h3"),
+    desc_section: document.createElement("p"),
+    age_section: document.createElement("p"),
+    book_section: document.createElement("div"),
+  };
+  return cardObj;
+}
 
-//Crear tarjeta, esta función sirve para hacer un request dato por dato y de ahí no se tienen que configurar las aplicaciones desde el HTML sino que se sacan desde la base de datos.
-const CARD = document.createElement("div");
-const NAME_SECTION = document.createElement("h3");
-const DECS_SECTION = document.createElement("p");
-const AGE_SECTION = document.createElement("p");
-const MUSIC_SECTION = document.createElement("div");
+// Crear tarjeta
+// Inyectamos info
+function injectData(obj, user) {
+  const bookList = user.fav_books.books.map((e) => {
+    const item = document.createElement("ul");
+    item.textContent = e;
+    return item;
+  });
 
-//Lista 
-    //Llama una función de tipo callback y regresa un array con los resultados, por cada libro voy a tener un elemento ul. 
-    const musiclist = USER.fav_music.music.map(e => {
-        //Coloca cada elemento en una ul y lo repite por cada item
-        const item = document.createElement("ul");
-        item.textContent = e;
-        return item;
-    });
-console.log(musiclist);
-//Crear tarjeta
-//TextContent e inner text son intercambiables. Nos sirven para extraer infortmación de nuestras constantes en la parte de arriba.
-NAME_SECTION.textContent = USER.userName;
-DECS_SECTION.textContent = USER.desc;
-AGE_SECTION.textContent = USER.age;
-//Lo que tengo en booklist es no es un nodo, es un arreglo. Es por ello que necesitamos los puntitos. Muestra todos los elementos con append. appendChild manda la operación sólo 1 vez, append lo hace con varios.
-MUSIC_SECTION.append(...musiclist);
+  obj.name_section.textContent = user.username;
+  obj.desc_section.textContent = user.desc;
+  obj.age_section.textContent = user.age;
+  obj.book_section.append(...bookList);
+  renderCard(obj);
+}
 
-//Push agarra nuestra colección de array y lo agrega hasta el final. Append hace lo mismo pero en nodos.
-CARD.append(NAME_SECTION, DECS_SECTION, AGE_SECTION, MUSIC_SECTION);
+function renderCard(cardObj) {
+  const card = document.createElement("div");
+  card.append(
+    cardObj.name_section,
+    cardObj.desc_section,
+    cardObj.age_section,
+    cardObj.book_section,
+  );
+  card.className = "div-creado";
+  CARDS_CONTAINER.appendChild(card);
+}
 
-//Inyectar tarjeta en el container
-CARDS_CONTAINER.appendChild(CARD);
+USERS.forEach((user) => {
+  const cardData = createElements();
+  injectData(cardData, user);
+});
